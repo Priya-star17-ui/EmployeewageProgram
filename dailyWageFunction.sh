ee28fa1fa90ecc445d964da69baec9585a6c72fc
Priya <priyadalai1393@gmail.com>
2020-05-25 14:53:55 +0530

    CalculatingDailyWagesUsingFunction

---
+++ dailyWageFunction.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HRS=20
+MAX_HRS_IN_MONTH=4
+NUM_WORKING_DAYS=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+
+function getWorkHrs()
+{
+         case $empCheck in
+        $IS_FULL_TIME)empHrs=8;;
+        $IS_PARK_TIME)empHrs=4;;
+        *)empHrs=0;;
+        esac
+        echo $empHrs
+ }
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        empHrs="$( getWorkHrs $empCheck )"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+        salary=$(( $totalEmpHrs * $EMP_RATE_PER_HRS ))
+        
